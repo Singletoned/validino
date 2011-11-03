56ab8c6333458db6970abbbed90d4af9784c3daf
Ed Singleton <singletoned@gmail.com>
2011-11-03 13:02:45 +0000

    Moved setup to be in a `if main` section

--- setup.py
+++ setup.py
@@ -16,20 +16,22 @@ Programming Language :: Python
 Topic :: Software Development :: Libraries :: Python Modules
 """.split('\n'))
 
-setup(author='Jacob Smullyan',
-      author_email='jsmullyan@gmail.com',
-      url='http://code.google.com/p/validino',
-      description=description,
-      long_description=long_description,
-      keywords=keywords,
-      platforms=platforms,
-      license='MIT',
-      name='validino',
-      version='0.3.0',
-      zip_safe=True,
-      include_package_data=True,
-      packages=['validino'],
-      package_dir={'' : 'src'},
-      test_suite='nose.collector',
-      )
-      
+__data__ = dict(
+    author='Jacob Smullyan',
+    author_email='jsmullyan@gmail.com',
+    url='http://code.google.com/p/validino',
+    description=description,
+    long_description=long_description,
+    keywords=keywords,
+    platforms=platforms,
+    license='MIT',
+    name='validino',
+    version='0.3',
+    zip_safe=True,
+    include_package_data=True,
+    packages=['validino'],
+    package_dir={'' : 'src'},
+    test_suite='nose.collector')
+
+if __name__ == '__main__':
+    setup(**__data__)
